bddc5330f98baca9a5978e1d1acd3c551a725164
tomas.kindahl@bahnhof.se <tomas.kindahl@bahnhof.se>
2025-04-23 07:24:15 +0200

    135.1.1 random script

---
+++ d135/135.1.1.blink.py
@@ -0,0 +1,22 @@
+# Random script example
+
+import time
+from machine import LED
+
+while True:
+    ledr.on()
+    ledg.off()
+    ledb.off()
+    time.sleep_ms(500)
+    ledr.off()
+    ledg.on()
+    ledb.off()
+    time.sleep_ms(500)
+    ledr.off()
+    ledg.off()
+    ledb.on()
+    time.sleep_ms(500)
+    ledr.off()
+    ledg.off()
+    ledb.off()
+    time.sleep_ms(500)
